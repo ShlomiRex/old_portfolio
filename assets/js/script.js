caca65639fc287d0cc85a7df4851ff86f71fdc02
Shlomi <vgtvgy1@gmail.com>
2023-07-09 17:18:42 +0300

    fix contact

--- assets/js/script.js
+++ assets/js/script.js
@@ -124,12 +124,12 @@ const formBtn = document.querySelector("[data-form-btn]");
 for (let i = 0; i < formInputs.length; i++) {
   formInputs[i].addEventListener("input", function () {
 
-    // check form validation
-    if (form.checkValidity()) {
-      formBtn.removeAttribute("disabled");
-    } else {
-      formBtn.setAttribute("disabled", "");
-    }
+    // // check form validation
+    // if (form.checkValidity()) {
+    //   formBtn.removeAttribute("disabled");
+    // } else {
+    //   formBtn.setAttribute("disabled", "");
+    // }
 
   });
 }
